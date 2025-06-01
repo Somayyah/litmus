bc30e066645b866b96dc4c8eeccc9685ed227817
watari <watari@techwebunraveled.xyz>
2025-06-01 13:03:55 +0300

    very high level overview don't judege

---
+++ litmus.sh
@@ -0,0 +1,20 @@
+
+test():
+	# Run the code and compare it to the test file
+	
+generate():
+	# To generate the test file(s)
+
+main():
+	# test of generate test file?
+	
+	# test
+		# Does c and JSON test files exist?
+			# Yes : test()
+			# No : Inform the user
+	# generate
+		# Does txt test file exist
+			# Yes : generate()
+			# No : Inform the user
+
+exit(0)
